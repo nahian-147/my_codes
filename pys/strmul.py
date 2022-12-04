c0aee41e270eadbe9be783236ce2c62f7404f0f8
kaziNAHIAN <provat44.knhp@gmail.com>
2022-12-05 03:49:31 +0600

    strmul updated

--- pys/strmul.py
+++ pys/strmul.py
@@ -1,12 +1,22 @@
-a = input("Enter a number, a = ")
-b = input("Enter another number, b = ")
+# a = input("Enter a number, a = ")
+# b = input("Enter another number, b = ")
+
+a="23108814760092"
+b="55183904456427981"
 mul = ''
 carry = 0
 for n in a[::-1]:
+    print(n,mul)
     p = int(n)*int(b) + carry
     mul += str((p)%10)
     carry = int(p/10)
 
-mul += str(carry)
+if carry != 0:
+    mul += str(carry)[::-1]
 
-print(mul[::-1])
+mul = mul[::-1]
+        
+while mul[0] == '0' and len(mul) > 1:
+    mul = mul[1:]
+
+print(mul)
